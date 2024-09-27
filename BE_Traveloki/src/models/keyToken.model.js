8d286a3661ee1699c83a688f7f4ebf998fbbc72c
quan3473 <nguyenminhquan042004@gmail.com>
2024-09-27 11:27:25 +0700

    .

--- BE_Traveloki/src/models/keyToken.model.js
+++ BE_Traveloki/src/models/keyToken.model.js
@@ -1,11 +1,40 @@
-'use strict'
+'use strict';
 
 const { Schema, model } = require('mongoose'); // Erase if already required
 
-// Declare the Schema of the Mongo model
-const keyTokenSchema = new Schema({
+const DOCUMENT_NAME = 'Key';
+const COLLECTION_NAME = 'Keys';
 
-});
+// Declare the Schema of the Mongo model
+const keyTokenSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'Shop',
+    },
+    privateKey: {
+      type: String,
+      required: true,
+    },
+    publicKey: {
+      type: String,
+      required: true,
+    },
+    refreshTokensUsed: {
+      type: Array,
+      default: [],
+    },
+    refreshToken: {
+      type: String,
+      require: true,
+    },
+  },
+  {
+    collection: COLLECTION_NAME,
+    timestamps: true,
+  }
+);
 
 //Export the model
-module.exports = model('key', keyTokenSchema);
+module.exports = model(DOCUMENT_NAME, keyTokenSchema);
--- BE_Traveloki/src/validations/user.validation.js
+++ BE_Traveloki/src/validations/user.validation.js
@@ -1,28 +1,37 @@
-export const loginUser = {
+const Joi = require('joi')
+
+const loginUser = {
   body: Joi.object().keys({
       email: Joi.string().email().required(),
       password: Joi.string().email().required()
   })
 }
 
-export const forgotPassword = {
+const forgotPassword = {
   body: Joi.object().keys({
       email: Joi.string().email().required()
   })
 }
 
-export const verifyOtp = {
+const verifyOtp = {
   body: Joi.object().keys({
       email: Joi.string().email().required(),
       otp: Joi.number().integer().required()
   })
 }
 
-export const resetPassword = {
+const resetPassword = {
   body: Joi.object().keys({
       password: Joi.string().required()
   }),
   query: Joi.object().keys({
       token: Joi.string()
   })
+}
+
+module.exports = {
+  loginUser,
+  forgotPassword,
+  verifyOtp,
+  resetPassword
 }
