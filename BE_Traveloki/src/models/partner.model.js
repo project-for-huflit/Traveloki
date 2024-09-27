931e4217af535b3db923b35358d5e27a9e2a66bf
tuan129 <maithaituan129@gmail.com>
2024-09-27 08:31:00 +0700

    auth

--- BE_Traveloki/src/models/partner.model.js
+++ BE_Traveloki/src/models/partner.model.js
@@ -1,49 +1,52 @@
-'use strict'
+'use strict';
 
-const { Schema, model } = require("mongoose");
+const { Schema, model } = require('mongoose');
 
-const partnerSchema = new Schema({
-  name: {
-    type: String,
-    trim: true,
-    maxLength: 150
+const partnerSchema = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      maxLength: 150,
+    },
+    description: {
+      type: String,
+    },
+    image: {
+      type: String,
+      default: null,
+    },
+    phone: {
+      type: String,
+    },
+    address: {
+      type: String,
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: [true, 'Email already exists!'],
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    privateKey: {
+      type: String,
+    },
+    publicKey: {
+      type: String,
+    },
+    status: {
+      type: String,
+      enum: ['active', 'inactive'],
+      default: 'inactive',
+    },
   },
-  description: {
-    type: String,
-  },
-  image: {
-    type: String,
-    default: null,
-  },
-  phone: {
-    type: String,
-  },
-  address: {
-    type: String,
-  },
-  email: {
-    type: String,
-    trim: true,
-    required: true,
-    unique: [true, "Email already exists!"],
-  },
-  password: {
-    type: String,
-    required:true,
-  },
-  privateKey: {
-    type: String,
-  },
-  publicKey: {
-    type: String,
-  },
-  status:{
-    type: String,
-    enum: ['active', 'inactive'],
-    default: 'inactive'
-  },
-}, {
+  {
     timestamps: true,
-});
+  }
+);
 
 module.exports = model('partner', partnerSchema);
