e7925a5811999adcc4eba9c984902969e95e8a64
quan3473 <nguyenminhquan042004@gmail.com>
2024-10-05 19:23:55 +0700

    .

--- BE_Traveloki/src/routes/history/historyCar-routes.js
+++ BE_Traveloki/src/routes/history/historyCar-routes.js
@@ -7,11 +7,12 @@ const {
   GetLichSuDatXeOto,
   createHistory,
   DeleteLichSuDatXeOto,
+  updateLichSuDatXeOto
 } = require("../../controllers/historyCar_controller");
 
 route.get("/GetLichSuDatXeOto", GetLichSuDatXeOto);
 route.post("/createHistory", createHistory);
 route.delete("/DeleteLichSuDatXeOto", DeleteLichSuDatXeOto);
-route.patch("/update-history-car/:id")
+route.patch("/update-history-car/:id", updateLichSuDatXeOto)
 
 module.exports = route;
