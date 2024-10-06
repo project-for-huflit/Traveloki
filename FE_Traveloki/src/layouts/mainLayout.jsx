aae36570fb2f8d21ab8246d5215171a6a1fd5bfb
quan3473 <nguyenminhquan042004@gmail.com>
2024-10-06 20:06:07 +0700

    Change new UI in Home page

--- FE_Traveloki/src/layouts/mainLayout.jsx
+++ FE_Traveloki/src/layouts/mainLayout.jsx
@@ -1,10 +1,10 @@
 import { Outlet } from "react-router-dom";
-import Header from "../components/layout/Header";
+import Header2 from "../components/layout/Header2";
 import Footer from "../components/layout/Footer";
 const MainLayout = () => {
   return (
     <div className="bg-[#F2F3F3]">
-      <Header />
+      <Header2 />
       <Outlet />
       <Footer />
     </div>
