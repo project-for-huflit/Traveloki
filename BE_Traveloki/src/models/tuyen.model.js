0672936ad1c81cd78648660d170043301e5d9df8
trungenglish <nguyentrung23232323@gmail.com>
2024-10-10 22:20:25 +0700

    update LichChay PhuongTien TramDung Tuyen Tuyen_TramDung

--- BE_Traveloki/src/models/tuyen.model.js
+++ BE_Traveloki/src/models/tuyen.model.js
@@ -3,8 +3,8 @@ const COLLECTION_NAME = 'Tuyens'
 
 const TuyenSchema = new Schema({
   MaTuyen: { type: String, required: true, maxlength: 5 },
-  DiemSanBay: { type: String, ref: "DanhSachSanBay", required: true },
-  DiemKetThuc: { type: String, maxlength: 300, required: true },
+  DiemKhoiHanh: { type: String, required: true },
+  DiemKetThuc: { type: String, required: true },
   ThoiGianKhoiHanh: { type: Date, required: true },
   ThoiGianKetThuc: { type: Date, required: true },
 },{
