6d42c9540ee3b5975ce302ee8db18a4a0295b7d6
quan3473 <nguyenminhquan042004@gmail.com>
2024-10-11 12:31:36 +0700

    .

--- BE_Traveloki/src/routes/auth/auth.js
+++ BE_Traveloki/src/routes/auth/auth.js
@@ -4,8 +4,11 @@ const express = require('express');
 const route = express.Router();
 
 const { login, register } = require('../../controllers/auth_controller');
+// const { loginSSO, registerSSO } = require('../../controllers/auth_controller');
 
-route.post('/register', register);
+route.post('/register', register)
 route.post('/login', login)
+// route.post('/register-sso', register)
+// route.post('/login-sso', login)
 
 module.exports = route;
