bf81389fd81359fe1ae240d9a8c7c67fbd42b922
tuan129 <maithaituan129@gmail.com>
2024-10-18 09:36:33 +0700

    UI Rating

--- FE_Traveloki/src/components/rating/StarRating.jsx
+++ FE_Traveloki/src/components/rating/StarRating.jsx
@@ -1,17 +1,16 @@
-import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 
 export const StarRating = ({ rating, setRating }) => {
   return (
-    <div className="flex space-x-1 justify-center">
+    <div className="gird gird-cols-5 place-content-center">
       {[1, 2, 3, 4, 5].map((star) => (
         <FontAwesomeIcon
           key={star}
           icon={faStar}
           onClick={() => setRating(star)}
-          className={`cursor-pointer ${
-            rating >= star ? "text-yellow-500" : "text-gray-400"
+          className={`cursor-pointer hover:text-yellow-300 h-16 w-16 rounded-full ${
+            rating >= star ? "text-yellow-300" : "text-gray-400"
           }`}
         />
       ))}
