14dbaf9bea3714b33ed2479cee35944c397a107d
quan3473 <nguyenminhquan042004@gmail.com>
2024-10-21 23:32:49 +0700

    Done payment booking car

--- FE_Traveloki/src/services/api/payment/index.js
+++ FE_Traveloki/src/services/api/payment/index.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const paymentSend =async (body)=>{
-  return await axios.post(import.meta.env.VITE_API_PRESSPAY_BASE_URL+'/api/v1/payment',
+  return await axios.post('https://presspay-api.azurewebsites.net/api/v1/payment',
     body, {
       headers:{
           Authorization: 'Bearer '+ import.meta.env.VITE_SECRET_API_KEY_POINTER
@@ -10,10 +10,10 @@ const paymentSend =async (body)=>{
   })
 }
 const PaymentGateway =async (token)=>{
-  return await axios.get(import.meta.env.VITE_API_PRESSPAY_BASE_URL+'/payment-gateway?token='+token)
+  return await axios.get('https://presspay-api.azurewebsites.net/payment-gateway?token='+token)
 }
 const paymentWithCard = async(body)=>{
-  return await axios.post(import.meta.env.VITE_API_PRESSPAY_BASE_URL+'/api/v1/payment-with-card',body)
+  return await axios.post('https://presspay-api.azurewebsites.net/api/v1/payment-with-card',body)
 }
 
 export {
