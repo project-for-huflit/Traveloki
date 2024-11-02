a5d9ab204a1f37cce9e251c5bdc1aa2167fd911c
quan3473 <nguyenminhquan042004@gmail.com>
2024-11-03 01:44:40 +0700

    Add function cancel payment wallet

--- BE_Traveloki/src/services/booking.service.js
+++ BE_Traveloki/src/services/booking.service.js
@@ -1,21 +1,39 @@
-'use strict'
-const { Pointer } = require("pointer-wallet");
+'use strict';
+const { Pointer } = require('pointer-wallet');
 
 const pointer = new Pointer(process.env.POINTER_SECRET_KEY);
 
 class BookingCarService {
-  static async PaymentPointerWallet ({
-    amount, currency, message, userID, orderID, returnUrl, name, image, description, quantity, price
+  static async PaymentPointerWallet({
+    amount,
+    currency,
+    message,
+    userID,
+    orderID,
+    returnUrl,
+    name,
+    image,
+    description,
+    quantity,
+    price,
   }) {
-
     // get userId
-
     // get bookingId
 
     /// ===================================================
-    console.log("Nhan thong tin payment::", {
-      amount, currency, message, userID, orderID, returnUrl, name, image, description, quantity, price
-    })
+    console.log('Nhan thong tin payment::', {
+      amount,
+      currency,
+      message,
+      userID,
+      orderID,
+      returnUrl,
+      name,
+      image,
+      description,
+      quantity,
+      price,
+    });
     const { url } = await pointer.createPayment({
       amount: amount,
       currency: currency,
@@ -30,27 +48,30 @@ class BookingCarService {
           description: description,
           quantity: quantity,
           price: price,
-        }
-      ]
+        },
+      ],
     });
     console.log(url);
-    return url
+    return url;
   }
 
-  static async CancelPaymentPointerWallet () {
-
+  /**
+    * @param transactionID: _id
+    * @returns { "url":"https://pointer.io.vn/payment-gateway?token={token}", "status":200 }
+    */
+  static async CancelPaymentPointerWallet({ _id }) {
+    const data = await pointer.cancelOrder(_id);
+    console.log(data);
+    return data
   }
 }
 
-class BookingBusService {
+class BookingBusService {}
 
-}
-
-class BookingTrainService {
-
-}
+class BookingTrainService {}
 
 module.exports = {
-  BookingCarService, BookingBusService, BookingTrainService
-}
-
+  BookingCarService,
+  BookingBusService,
+  BookingTrainService,
+};
