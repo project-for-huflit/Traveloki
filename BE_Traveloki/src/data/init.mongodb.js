d3ae1acd7078e0fd202d4777f6a1b53222481f74
quan3473 <nguyenminhquan042004@gmail.com>
2025-03-08 13:19:21 +0700

    Push package-lock

--- BE_Traveloki/src/data/init.mongodb.js
+++ BE_Traveloki/src/data/init.mongodb.js
@@ -16,53 +16,53 @@ mongoose
 
 module.exports = mongoose;
 
-// class Database {
-//   constructor() {
-//       this.connect();
-//   }
-//
-//   // connect
-//   connect(type = 'mongodb') {
-//     if(1 === 0){
-//         mongoose.set('debug', true)
-//         mongoose.set('debug', { color: true })
-//     }
-//
-//     mongoose
-//         .connect(connectString, { maxPoolSize: 50 })
-//         .then(() => {
-//             console.log("Connected to MongoDB - main!", countConnect());
-//         })
-//         .catch((e) => {
-//             console.error("Did not connect to MongoDB", e);
-//         });
-//
-//     mongoose.connection.on('reconnected', () => {
-//       console.log('Connection Restablished')
-//     })
-//
-//     mongoose.connection.on('disconnected', () => {
-//       console.log('Connection Disconnected')
-//     })
-//
-//     mongoose.connection.on('close', () => {
-//       console.log('Connection Closed')
-//     })
-//   }
-//
-//   // singleton pattern
-//   static getInstance() {
-//     if (!Database.instance) {
-//       Database.instance = new Database();
-//     }
-//     return Database.instance;
-//   }
-//
-//   static getConnect() {
-//     return this.getInstance();
-//   }
-// }
-//
-// const instanceMongoDB = Database.getInstance();
-// const cloudMongoDB = Database.getConnect()
-// module.exports = instanceMongoDB;
+class Database {
+  constructor() {
+      this.connect();
+  }
+
+  // connect
+  connect(type = 'mongodb') {
+    if(1 === 0){
+        mongoose.set('debug', true)
+        mongoose.set('debug', { color: true })
+    }
+
+    mongoose
+        .connect(process.env.MONGODB_URI_2, { maxPoolSize: 50 })
+        .then(() => {
+            console.log("Connected to MongoDB - main!", countConnect());
+        })
+        .catch((e) => {
+            console.error("Did not connect to MongoDB", e);
+        });
+
+    mongoose.connection.on('reconnected', () => {
+      console.log('Connection Restablished')
+    })
+
+    mongoose.connection.on('disconnected', () => {
+      console.log('Connection Disconnected')
+    })
+
+    mongoose.connection.on('close', () => {
+      console.log('Connection Closed')
+    })
+  }
+
+  // singleton pattern
+  static getInstance() {
+    if (!Database.instance) {
+      Database.instance = new Database();
+    }
+    return Database.instance;
+  }
+
+  static getConnect() {
+    return this.getInstance();
+  }
+}
+
+const instanceMongoDB = Database.getInstance();
+const cloudMongoDB = Database.getConnect()
+module.exports = instanceMongoDB;
